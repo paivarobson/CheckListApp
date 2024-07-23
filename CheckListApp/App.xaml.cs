b2f63c346d165667b99fb6a3e5ec975526a83c5d
Robson <paivawork@hotmail.com>
2024-07-23 15:31:03 -0300

    Criacao do projeto

---
+++ CheckListApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CheckListApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
+
---
+++ CheckListApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CheckListApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
+
